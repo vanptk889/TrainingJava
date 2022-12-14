5511a8947fb0b3dc8bb2a9ceeb46d08003636914
van.phamkieu <vanptk22@wru.vn>
2022-12-14 16:49:40 +0700

    van commit

---
+++ src/main/java/hong/buoi1/OrderInfo.java
@@ -0,0 +1,22 @@
+package hong.buoi1;
+
+public class OrderInfo {
+    private String name;
+    private String pass;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
