4c2628b7977be8b47c9d7d5870b1d3098db75e68
dragon98hn <linhdohoang89@gmail.com>
2022-12-23 11:23:38 +0700

    Merge remote-tracking branch 'origin/linhhoang' into linhhoang

--- src/main/java/linhhoang/BTBuoi4/BT4.java
+++ src/main/java/linhhoang/BTBuoi4/BT4.java
@@ -2,27 +2,36 @@ package linhhoang.BTBuoi4;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.*;
+import java.util.Set;
 
 public class BT4 {
     public static void main(String[] args) {
         Map<String, OrderInfo> map = new HashMap<>();
         OrderInfo orderInfo = new OrderInfo();
-        orderInfo.setSymbol1("TPB");
-        orderInfo.setPrice1(41.5f);
-        orderInfo.setQuantity1(500);
-        orderInfo.setMatchedPrice1(41.8f);
-        orderInfo.setStatus1("Filled");
+        orderInfo.setSymbol("TPB");
+        orderInfo.setPrice(41.5f);
+        orderInfo.setQuantity(500);
+        orderInfo.setMatchedPrice(41.8f);
+        orderInfo.setStatus("Filled");
         map.put("TC01", orderInfo);
 
-        orderInfo.setSymbol2("VND");
-        orderInfo.setPrice2(36.8f);
-        orderInfo.setQuantity2(1000);
-        orderInfo.setMatchedPrice2(36.5f);
-        orderInfo.setStatus2("Partially Filled");
+        orderInfo = new OrderInfo();
+        orderInfo.setSymbol("VND");
+        orderInfo.setPrice(36.8f);
+        orderInfo.setQuantity(1000);
+        orderInfo.setMatchedPrice(36.5f);
+        orderInfo.setStatus("Partially Filled");
         map.put("TC02", orderInfo);
 
-        for (String i : map.keySet()) {
-            System.out.println("key: " + i + " value: " + map.get(i));
+        System.out.println("Symbol, "+"Giá đặt, "+"Khối lượng, "+"Giá khớp, "+"Trạng thái");
+
+        Set<Map.Entry<String, OrderInfo>> set = map.entrySet();
+        for (Map.Entry<String, OrderInfo> orderInfoEntry : set ){ //orderInfoEntry là map
+            String key = orderInfoEntry.getKey(); //hàm getKey của map
+            OrderInfo value = orderInfoEntry.getValue(); //gán giá trị cho đối tượng value, ko cần khới tạo đối tượng value nữa. value là đối tượng
+            System.out.println(key+", "+value.getPrice()+", "+value.getQuantity()+", "+value.getMatchedPrice()+", "+value.getStatus());
+
         }
     }
 }
--- src/main/java/linhhoang/BTBuoi4/OrderInfo.java
+++ src/main/java/linhhoang/BTBuoi4/OrderInfo.java
@@ -1,94 +1,49 @@
 package linhhoang.BTBuoi4;
 
 public class OrderInfo {
-    private String symbol1;
-    private String symbol2;
-    private float price1;
-    private float price2;
-    private float matchedPrice1;
-    private float matchedPrice2;
-    private int quantity1;
-    private int quantity2;
-    private String status1;
-    private String status2;
+    private String symbol;
+    private float price;
+    private float matchedPrice;
+    private int quantity;
+    private String status;
 
-    public String getSymbol1() {
-        return symbol1;
+    public String getSymbol() {
+        return symbol;
     }
 
-    public void setSymbol1(String symbol1) {
-        this.symbol1 = symbol1;
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
     }
 
-    public String getSymbol2() {
-        return symbol2;
+    public float getPrice() {
+        return price;
     }
 
-    public void setSymbol2(String symbol2) {
-        this.symbol2 = symbol2;
+    public void setPrice(float price) {
+        this.price = price;
     }
 
-    public float getPrice1() {
-        return price1;
+    public float getMatchedPrice() {
+        return matchedPrice;
     }
 
-    public void setPrice1(float price1) {
-        this.price1 = price1;
+    public void setMatchedPrice(float matchedPrice) {
+        this.matchedPrice = matchedPrice;
     }
 
-    public float getPrice2() {
-        return price2;
+    public int getQuantity() {
+        return quantity;
     }
 
-    public void setPrice2(float price2) {
-        this.price2 = price2;
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 
-    public float getMatchedPrice1() {
-        return matchedPrice1;
+    public String getStatus() {
+        return status;
     }
 
-    public void setMatchedPrice1(float matchedPrice1) {
-        this.matchedPrice1 = matchedPrice1;
-    }
-
-    public float getMatchedPrice2() {
-        return matchedPrice2;
-    }
-
-    public void setMatchedPrice2(float matchedPrice2) {
-        this.matchedPrice2 = matchedPrice2;
-    }
-
-    public int getQuantity1() {
-        return quantity1;
-    }
-
-    public void setQuantity1(int quantity1) {
-        this.quantity1 = quantity1;
-    }
-
-    public int getQuantity2() {
-        return quantity2;
-    }
-
-    public void setQuantity2(int quantity2) {
-        this.quantity2 = quantity2;
-    }
-
-    public String getStatus1() {
-        return status1;
-    }
-
-    public void setStatus1(String status1) {
-        this.status1 = status1;
-    }
-
-    public String getStatus2() {
-        return status2;
-    }
-
-    public void setStatus2(String status2) {
-        this.status2 = status2;
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
