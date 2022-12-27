95c34cb2f8e2753f83ba0ed76ac73635b434a62e
ngocphuong42 <btngocphuong.s@gmail.com>
2022-12-28 00:06:49 +0700

    exercise 5

---
+++ src/main/java/B4/B4_exercise5.java
@@ -0,0 +1,26 @@
+package B4;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class B4_exercise5 {
+    public static void main(String[] args) {
+        Map<String, String> account= new LinkedHashMap<String, String>();
+        account.put("thuyquynh","12345678");
+        account.put("vanpham","23457890");
+        print(account);
+        //change password
+        account.replace("thuyquynh","123456789");
+        System.out.println("Danh sach sau khi update la: ");
+        print(account);
+    }
+    public static void print(Map<String, String> mapName){
+        Set<Map.Entry<String, String>> set = mapName.entrySet();
+        for (Map.Entry<String, String> map : set ){
+            String key = map.getKey();
+            String value = map.getValue();
+            System.out.println("username: "+key+" ,password: "+value);
+        }
+    }
+}
